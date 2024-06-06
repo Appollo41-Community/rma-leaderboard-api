4bfcbbe40c1db02979cd5c4787de9535c534090b
Aleksandar Ilic <aleksandar@appollo41.com>
2024-06-06 17:50:56 +0200

    Fix sample test

--- src/test/kotlin/rs/raf/edu/rma/ApplicationTest.kt
+++ src/test/kotlin/rs/raf/edu/rma/ApplicationTest.kt
@@ -1,18 +1,14 @@
-package com.appollo41.ai
+package rs.raf.edu.rma
 
 import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
-import rs.raf.edu.rma.plugins.routing.configureRouting
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
-        application {
-            configureRouting()
-        }
         client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
         }
